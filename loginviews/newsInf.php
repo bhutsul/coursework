0d7eddcf815c9b91ea543e51ea4c5f8fd061fe05
bohdan <bogdanhgf56@gmail.com>
2019-04-01 17:59:18 +0300

    dynamic reload photo

---
+++ loginviews/newsInf.php
@@ -0,0 +1,33 @@
+    <?php
+        session_start();
+        require("../config/db.php");
+
+        $startFrom = $_POST['newsStartFrom'];
+        $endFrom   = $_POST['newsEndFrom'];
+        $id        = $_SESSION['id'];
+
+        $getUserFollow = mysqli_query($link,"SELECT id_user_follow FROM followers where id_follower = '$id'");
+        $news = [];
+
+        while ( $rowUserFollow = mysqli_fetch_assoc($getUserFollow) )
+        {
+          $idUserNews = $rowUserFollow['id_user_follow'];
+
+          $getInfFollow = mysqli_query($link, "SELECT * FROM `imggallery` INNER JOIN users ON imggallery.iduser = users.id where iduser = '$idUserNews' ");
+
+
+          while ( $infFollow = mysqli_fetch_assoc($getInfFollow) )
+          {
+            $news[] = $infFollow;
+          }
+        }
+        $count = count($news);
+        $newsStart = [];
+
+        for ($i = $startFrom; $i < $endFrom; $i++)
+        {
+          $newsStart[] = $news[$i];
+        }
+
+        echo '<JSON>'.json_encode($newsStart). '</JSON>';
+    ?>
